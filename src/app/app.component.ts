557b3a72410ba28061a73059f2e105fcfb7f6fc1
Hugo Vieira <hgovra@gmail.com>
2022-08-10 20:08:27 -0300

    Adicionado serviço que gera palavras aleatórias.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import wordList from 'word-list-json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dictionary';
+
+  get randomWord(): string {
+    const index = Math.floor(Math.random() * wordList.length);
+    
+    return wordList[index];
+  }
+
+  words: string[] = [];
+
+  generateWordList(amount = 12) {
+    for(let i = 0; i < amount; i++) {
+      this.words.push(this.randomWord);
+    }
+  }
+
+  ngOnInit() {
+    
+    this.generateWordList();
+      console.log(this.words);
+    //});
+  }
 }
